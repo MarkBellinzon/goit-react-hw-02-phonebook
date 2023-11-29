5d98d07a482a437d65daec70675875fcd2bffbd7
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-11-29 14:24:30 +0200

    section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,48 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
+
+  addContact = data => {
+    const { contacts } = this.state;
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+
+    contacts.some(({ name }) => name === data.name)
+      ? alert(`${data.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+  };
+
+  // render() {
+  //   return (
+  //     <>
+  //       <Section>
+  //         <ContactForm />
+  //       </Section>
+  //       <Section>
+  //         <Filter />
+
+  //         <ContactList />
+  //       </Section>
+  //     </>
+  //   );
+  // }
 }
